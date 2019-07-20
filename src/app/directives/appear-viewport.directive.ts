6595f8a0d0e6d413aae2e2d33e64041c63f42629
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-07-20 10:26:01 +0200

    Fixed appear-viewport directive with Angular Material

---
+++ src/app/directives/appear-viewport.directive.ts
@@ -0,0 +1,41 @@
+import { Directive, ElementRef, HostListener, EventEmitter, Output, AfterViewInit } from '@angular/core';
+import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/scrolling';
+import { empty } from 'rxjs';
+
+@Directive({
+    selector: '[appear]'
+})
+export class AppearViewPortDirective implements AfterViewInit {
+  fired = false;
+  @Output() appear: EventEmitter<void> = new EventEmitter<any>();
+
+  constructor(public el: ElementRef, private scrollDispatcher: ScrollDispatcher) {
+
+    this.scrollDispatcher.scrolled().subscribe((x: CdkScrollable) => {
+      if ( !this.fired && this.isAnyPartOfElementInViewport(this.el)  ) {
+        this.fired = true;
+        this.appear.emit();
+      }
+    });
+  }
+  ngAfterViewInit() {
+    if (!this.fired && this.isAnyPartOfElementInViewport(this.el)) {
+      this.fired = true;
+      this.appear.emit();
+    }
+  }
+  isAnyPartOfElementInViewport(el: ElementRef ) {
+
+    const rect = el.nativeElement.getBoundingClientRect();
+    // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+    const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+    // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+    const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+    const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+    console.log('inviewport?', vertInView && horInView);
+    return (vertInView && horInView);
+  }
+
+}

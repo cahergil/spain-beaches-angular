5d6ee1e1fdc35b614d98253ec7e961eeb57cdd80
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-10-11 08:46:44 +0200

    Add tests to ResultContentComponent

--- src/app/landing-page/map-results/results-content/results-content.component.spec.ts
+++ src/app/landing-page/map-results/results-content/results-content.component.spec.ts
@@ -1,25 +1,97 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultsContentComponent } from './results-content.component';
+import { Playa } from 'src/app/playas.model';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  SimpleChange,
+  NO_ERRORS_SCHEMA
+} from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { ResultsContentItemComponent } from './results-content-item/results-content-item.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('ResultsContentComponent', () => {
+fdescribe('ResultsContentComponent', () => {
   let component: ResultsContentComponent;
   let fixture: ComponentFixture<ResultsContentComponent>;
-
+  let regionalBeachesList: Playa[];
+  let beachesList: Playa[];
+  beforeAll(async () => {
+    try {
+      const response = await fetch('../../../../assets/playas.json');
+      beachesList = await response.json();
+      regionalBeachesList = beachesList.filter(
+        (b: Playa) => b.comunidad_autonoma === 'Cantabria'
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  });
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ResultsContentComponent ]
-    })
-    .compileComponents();
+      declarations: [ResultsContentComponent, ResultsContentItemComponent],
+      // ResultContentItem image directive, not ok with CUSTOM_ELEMENTS_SCHEMA
+      schemas: [NO_ERRORS_SCHEMA],
+      // ResultContentItem has a router.navigate
+      imports: [RouterTestingModule.withRoutes([])]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ResultsContentComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.list = regionalBeachesList;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+  it('should show correct lengths in shows', () => {
+    component.list = regionalBeachesList;
+    fixture.detectChanges();
+    component.ngOnChanges({
+      list: {
+        previousValue: [],
+        currentValue: regionalBeachesList,
+        firstChange: false,
+        isFirstChange: () => false
+      }
+    });
+    // without this line fails the partial check
+    fixture.detectChanges();
+    const partialSpan = fixture.debugElement.query(
+      By.css('.results__showing span:nth-child(1)')
+    );
+    const totalSpan = fixture.debugElement.query(
+      By.css('.results__showing span:nth-child(2)')
+    );
+    expect(partialSpan.nativeElement.textContent).toContain(
+      component.getStep()
+    );
+    expect(totalSpan.nativeElement.textContent).toContain(
+      component.list.length
+    );
+  });
+  it('should render first 20(step) beaches of the beaches list', () => {
+    component.list = regionalBeachesList;
+    fixture.detectChanges();
+    component.ngOnChanges({
+      list: {
+        previousValue: [],
+        currentValue: regionalBeachesList,
+        firstChange: false,
+        isFirstChange: () => false
+      }
+    });
+    // necessary to detect ngOnChanges
+    fixture.detectChanges();
+    const dEls = fixture.debugElement.queryAll(
+      By.directive(ResultsContentItemComponent)
+    );
+    const fakeBeaches = regionalBeachesList.slice(0, 20);
+    for (let i = 0; i < fakeBeaches.length; i++) {
+      expect(dEls[i].componentInstance.beachItem).toEqual(fakeBeaches[i]);
+    }
   });
 });
--- src/app/landing-page/map-results/results-content/results-content.component.ts
+++ src/app/landing-page/map-results/results-content/results-content.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit, Input, ChangeDetectorRef, AfterViewInit, OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectorRef,
+  AfterViewInit,
+  OnChanges,
+  SimpleChanges,
+  OnDestroy
+} from '@angular/core';
 import { ScrollDispatcher, CdkScrollable } from '@angular/cdk/scrolling';
 
 import { Playa } from 'src/app/playas.model';
@@ -10,30 +19,34 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./results-content.component.scss']
 })
 export class ResultsContentComponent implements OnInit, OnChanges, OnDestroy {
-
   public filteredList: Playa[] = [];
   private offset: number;
   private step = 20;
   private reachedEnd = false;
   @Input() list: Playa[];
   private subscription: Subscription;
-  constructor(private ref: ChangeDetectorRef, private scrollDispatcher: ScrollDispatcher) {
+  constructor(
+    private ref: ChangeDetectorRef,
+    private scrollDispatcher: ScrollDispatcher
+  ) {
     // https://stackoverflow.com/questions/44516017/how-to-handle-window-scroll-event-in-angular-4/54005183#54005183
     // https://material.angular.io/cdk/scrolling/overview
-    this.subscription = this.scrollDispatcher.scrolled().subscribe((x: CdkScrollable) => {
-      const val = x.measureScrollOffset('bottom');
-      if (val < 100) {
-       this.onScroll();
-      }
-    });
+    this.subscription = this.scrollDispatcher
+      .scrolled()
+      .subscribe((x: CdkScrollable) => {
+        const val = x.measureScrollOffset('bottom');
+        if (val < 100) {
+          this.onScroll();
+        }
+      });
   }
 
-
-  ngOnInit() {
-
+  getStep(): number {
+    return this.step;
   }
+  ngOnInit() {}
 
-  loadData() {
+  loadData(): void {
     // offset init should be here, for each new region click
     this.offset = 0;
     this.filteredList = this.list.slice(this.offset, this.step);
@@ -53,9 +66,12 @@ export class ResultsContentComponent implements OnInit, OnChanges, OnDestroy {
       this.offset += this.step;
       // when refreshing the page , the first thing to come is an empty array [], this makes the variable reachedEnd= true
       // to avoid that add the following: && this.list.length !== 0
-    } else if (this.offset + this.step > this.list.length && this.list.length !== 0) {
-      console.log(this.list.length + ',' + this.offset);
-      this.offset += (this.list.length - this.offset);
+    } else if (
+      this.offset + this.step > this.list.length &&
+      this.list.length !== 0
+    ) {
+      // console.log(this.list.length + ',' + this.offset);
+      this.offset += this.list.length - this.offset;
       this.reachedEnd = true;
     }
     this.filteredList = [...this.list.slice(0, this.offset)];

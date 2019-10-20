d4621d54b20af8a570abe46544adf5bed8d7bd06
Carlos Hernández Gil <chernandezgil@gmail.com>
2019-10-20 13:00:46 +0200

    Finish tests in AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -7,12 +7,15 @@ import {
   tick,
   fakeAsync
 } from '@angular/core/testing';
-import { StoreModule } from '@ngrx/store';
+import { StoreModule, Store } from '@ngrx/store';
 import { MockComponents } from 'ng-mocks';
 import { of, Observable } from 'rxjs';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { Router, Routes } from '@angular/router';
 
 import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
@@ -21,7 +24,29 @@ import { HeaderComponent } from './navigation/header/header.component';
 import { BeachesService } from './beaches.service';
 import { Playa } from './playas.model';
 import { reducers } from './store/app.reducers';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-search',
+  template: ''
+})
+export class SearchComponent {}
+@Component({
+  selector: 'app-about',
+  template: ''
+})
+export class AboutComponent {}
+
+@Component({
+  selector: 'app-landing-page',
+  template: ''
+})
+export class LandingPageComponent {}
+const routes: Routes = [
+  { path: 'spain-map', component: LandingPageComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 fdescribe('AppComponent', () => {
   let component: AppComponent;
@@ -46,14 +71,17 @@ fdescribe('AppComponent', () => {
       declarations: [
         AppComponent,
         HeaderComponent,
-        SidenavListComponent
+        SidenavListComponent,
+        LandingPageComponent,
+        SearchComponent,
+        AboutComponent
         // MockComponents(SidenavListComponent)
         // MockComponents(SidenavListComponent, HeaderComponent)
       ],
       imports: [
         StoreModule.forRoot(reducers),
         MaterialModule,
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(routes),
         NoopAnimationsModule,
         FlexLayoutModule
       ],
@@ -76,7 +104,14 @@ fdescribe('AppComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  fit('should open the sidenav when clicking burger icon in toolbar', async () => {
+  it('should dispatch action in ngOnInit', () => {
+    const store = TestBed.get(Store);
+    const spy = spyOn(store, 'dispatch');
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should open the sidenav when clicking burger icon in toolbar', async () => {
     viewport.set(599);
     fixture.detectChanges();
     // const headerComponent: HeaderComponent = fixture.debugElement.query(
@@ -94,11 +129,12 @@ fdescribe('AppComponent', () => {
     await fixture.whenStable();
     expect(component.sidenav.opened).toBe(true);
   });
-  fit('should close the sidenav when clicking on each section', async () => {
+  it('should close the sidenav when clicking on each section', async () => {
     viewport.set(599);
     fixture.detectChanges();
     expect(component.sidenav.opened).toBe(false);
     const headerDe = fixture.debugElement.query(By.directive(HeaderComponent));
+    // get burger icon
     const buttonDe = headerDe.query(
       By.css('mat-toolbar > div:nth-child(1) > button')
     );
@@ -109,7 +145,9 @@ fdescribe('AppComponent', () => {
     const sidenavDe = fixture.debugElement.query(
       By.directive(SidenavListComponent)
     );
+    // get links
     const links = sidenavDe.queryAll(By.css('a'));
+    // click on each link
     // tslint:disable-next-line: prefer-for-of
     for (let i = 0; i < links.length; i++) {
       links[i].nativeElement.click();
@@ -120,4 +158,38 @@ fdescribe('AppComponent', () => {
       expect(component.sidenav.opened).toBe(true);
     }
   });
+  fit('can navigate to spain-map', fakeAsync(() => {
+    const router: Router = TestBed.get(Router);
+    const location = TestBed.get(Location);
+    fixture.ngZone.run(() => {
+      router.navigate(['/spain-map']);
+      // execute all pending asynchronous calls
+      // 	We wait for all pending promises to be resolved.
+      tick();
+      fixture.detectChanges();
+      expect(location.path()).toBe('/spain-map');
+    });
+  }));
+  fit('can navigate to search', fakeAsync(() => {
+    const router: Router = TestBed.get(Router);
+    const location = TestBed.get(Location);
+    fixture.ngZone.run(() => {
+      router.navigate(['/search']);
+      // execute all pending asynchronous calls
+      // 	We wait for all pending promises to be resolved.
+      tick();
+      fixture.detectChanges();
+      expect(location.path()).toBe('/search');
+    });
+  }));
+  fit('can navigate to about', fakeAsync(() => {
+    const router: Router = TestBed.get(Router);
+    const location = TestBed.get(Location);
+    fixture.ngZone.run(() => {
+      router.navigate(['/about']);
+      tick();
+      fixture.detectChanges();
+      expect(location.path()).toBe('/about');
+    });
+  }));
 });
